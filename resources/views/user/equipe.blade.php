cb71b1d45006245e2ccabbf4503f42bfa835e751
Arthaud Proust <arthaudp33@gmail.com>
2021-04-29 13:25:21 +0200

    fix about

--- resources/views/user/equipe.blade.php
+++ resources/views/user/equipe.blade.php
@@ -6,10 +6,10 @@
     </x-slot>
 
     <x-view.section>
-        <div class="text-2xl mb-12">
+        <div class="text-xl md:text-2xl mb-12">
             Nous sommes de jeunes Bordelais et Bordelaises qui apportons de l'évènementiel à la ville en organisant des concours photo aux thèmes variés.
         </div>
-        <div class="pt-12 flex flex-row flex-wrap items-center justify-center">
+        <div class="pt-12 flex flex-row flex-wrap items-center justify-around">
             @php
             $a=0.6;
             $h=0.6;
@@ -21,9 +21,9 @@
             @foreach($users as $user)
                 <x-view.profilcard style="animation-delay: {{0.5+$k+$a*sin(($loop->index-$h)/$b)}}s;" class="opacity-0 fade-in" src="{{ asset($user->img) }}" :title="$user->name" :subtitle="$user->bio" />
             @endforeach
-            <!-- @for($i=0; $i<=10;$i++)
+            @for($i=0; $i<=10;$i++)
             <x-view.profilcard style="animation-delay: {{1+$k+$a*sin(($i-$h)/$b)}}s;" class="opacity-0 fade-in" src="{{ asset('/assets/equipe/arthaud.jpg') }}" title="Arthaud Proust" subtitle="Jury" />
-            @endfor -->
+            @endfor
         </div>
     </x-view.section>
 </x-app-layout>
